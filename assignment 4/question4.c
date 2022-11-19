0199b4332696bf687b3ea145036172ecdcc16bff
Satyam kushwaha <stymkushwaha123@gmail.com>
2022-11-19 07:56:19 +0530

    version 1

---
+++ assignment 4/question4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for ( i = 1; i <= 10; i++)
+    {
+        printf("%d\n",i*2-1);
+    }
+    
+    return 0;
+}
---
+++ assignment 4/question5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for ( i = 10; i >=1; i--)
+    {
+        printf("%d\n",i*2-1);
+    }
+    
+    return 0;
+}
